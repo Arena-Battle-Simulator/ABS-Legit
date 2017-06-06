22108385ae5efeeeb18659c6a670a199358c0446
568668 <568668@2452IR306143480.ad1.peelsb.com>
2017-06-06 08:33:11 -0400

    committing new moves class

--- ABS/src/Moves.java
+++ ABS/src/Moves.java
@@ -1,37 +1,40 @@
+//class to read in and store all data relating to types and moves
 import java.io.*;
 import java.util.Scanner;
 import java.awt.*;
 
 public class Moves {
-
-	String skip;
-	String [] [] moveNames = new String [8] [11];
-	int [] [] moveValues = new int [80] [6];
-	int [] [] moveStats = new int [8] [2];
+	String [] [] moveNames = new String [8] [11]; //stores name of types and name of moves
+	int [] [] moveValues = new int [80] [6]; //stores all six statistics of every move (dmg, stamDMG, stam cost, hp cost, heal, stam heal in that order)
+	int [] [] moveStats = new int [8] [2]; //stores base health and stam of every type
+	
 	
 	public void moveRead (){
+	
 	try{ 
-	File data = new File("mov.txt");
-	Scanner in = new Scanner(data);
+	File data = new File("moves.txt"); //txt file to read from
+	Scanner in = new Scanner(data); //creating scanner named "data"
 	
 	int x = 0;
-	while (x < 8){
-	moveNames[x][0]=in.next();
-	//System.out.println("moveNames[x][0]");
+	while (x < 8){ //loops 8 times
+	moveNames[x][0]=in.next(); //storing the name of each type as the first value
+	System.out.println(moveNames[x][0]); //testing (remove later)
 	x++;
 	}
 	
 	x = 0;
-	while (x < 8){
-	moveStats[x][0]= in.nextInt();
-	moveStats[x][1]= in.nextInt();
+	while (x < 8){ //loops 8 times
+	moveStats[x][0]= in.nextInt(); //stores base health
+	moveStats[x][1]= in.nextInt(); //stores base stamina
+	System.out.println(moveStats[x][0]); //testing (remove later)
 	}
 	
 	x = 0;
-	int y = 1;
-	while (x < 8){
-	while (y < 11){
-		moveNames [x][y]=in.next();
+	int y = 1; //start at 1 because 0 is already storing name of type
+	while (x < 8){ //loops 8 times (one for every type)
+	while (y < 11){ //loops 10 times (one for every move per type)
+		moveNames [x][y]=in.next(); //storing the names of the moves
+		System.out.println(moveNames[x][y]); //testing (remove later)
 		y++;
 	}
 	x++;
@@ -39,18 +42,17 @@ public class Moves {
 	
 	x = 0;
 	y = 0;
-	while (x < 80){
-	while (y < 6){
-		moveValues[x][y]=in.nextInt();	
+	while (x < 80){ //loops 80 times (one for each move)
+	while (y < 6){ //loops 6 times (one for each statistic [dmg, heal, cost, etc])
+		moveValues[x][y]=in.nextInt(); //stores each move's statistics
+		System.out.println(moveValues); //testing (remove later)
 		y++;
 	}
-	skip = in.next();
 	x++;
 	}
 	
 	}
-	catch(FileNotFoundException e){
-		
+	catch(FileNotFoundException e){		
 	}
 	}
 }
